746e175a10a56d5f0dc3a3372c2d0ce4bb2ce57a
Rom Midnight <rmargve@gmail.com>
2020-08-27 17:14:50 +0400

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
   },
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
